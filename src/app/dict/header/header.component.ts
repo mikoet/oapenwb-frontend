f1e6a1ab53cbffd5fdf8b3ce22a0919dc7954aae
mikoet <mkoether38@gmail.com>
2022-09-25 01:30:51 +0200

    Dict: added hover effect to header links and active lang as parameter to routing pipe so it's working correctly after a language switch

--- src/app/dict/header/header.component.ts
+++ src/app/dict/header/header.component.ts
@@ -1,7 +1,9 @@
 // SPDX-FileCopyrightText: © 2022 Michael Köther <mkoether38@gmail.com>
 // SPDX-License-Identifier: AGPL-3.0-only
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { TranslocoService } from '@ngneat/transloco';
+
+import { faLanguage } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
 	selector: 'dict-header',
@@ -9,11 +11,12 @@ import { TranslocoService } from '@ngneat/transloco';
 	styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
+	faLanguage = faLanguage;
 	hideMenu: boolean = true;
 
 	constructor(
 		public transloco: TranslocoService,
+		private changeDetector: ChangeDetectorRef
 	) { }
 
 	ngOnInit(): void {
