ec04d3a9d6e7ad966e0a991c1fdce07e52217f5b
mikoet <mkoether38@gmail.com>
2022-10-07 00:27:14 +0200

    Shared: added MarkdownComponent

--- src/app/shared/_components/lemma-component/lemma-component.component.ts
+++ src/app/shared/_components/lemma-component/lemma-component.component.ts
@@ -1,7 +1,6 @@
 // SPDX-FileCopyrightText: © 2022 Michael Köther <mkoether38@gmail.com>
 // SPDX-License-Identifier: AGPL-3.0-only
 import { Component, Input, OnInit } from '@angular/core';
-import { TRANSLOCO_SCOPE } from '@ngneat/transloco';
 
 @Component({
 	selector: 'lemma',
