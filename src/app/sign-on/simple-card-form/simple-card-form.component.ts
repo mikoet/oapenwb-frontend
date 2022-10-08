1443e153f59fead53661fa67b415510f957505e0
mikoet <mkoether38@gmail.com>
2022-10-08 22:05:59 +0200

    SignOn: Added owl logo and home link to SimpleCardFormComponent. Commented out intro text on RegisterComponent.

--- src/app/sign-on/simple-card-form/simple-card-form.component.ts
+++ src/app/sign-on/simple-card-form/simple-card-form.component.ts
@@ -1,16 +1,16 @@
 // SPDX-FileCopyrightText: © 2022 Michael Köther <mkoether38@gmail.com>
 // SPDX-License-Identifier: AGPL-3.0-only
 import { Component, OnInit } from '@angular/core';
+import { TranslocoService } from '@ngneat/transloco';
 
 @Component({
   selector: 'simple-card-form',
   templateUrl: './simple-card-form.component.html',
   styleUrls: ['./simple-card-form.component.scss']
 })
-export class SimpleCardFormComponent implements OnInit
+export class SimpleCardFormComponent
 {
-	constructor() { }
-
-	ngOnInit(): void {
-	}
+	constructor(
+		public transloco: TranslocoService,
+	) { }
 }
