4b9666a253f7b47f7c94f416d7445980c65ec852
mikoet <mkoether38@gmail.com>
2022-12-30 03:14:05 +0100

    Made use of global variable DEFAULT_UI_LOCALE

--- src/app/dict/table-view/result-table/result-table.component.ts
+++ src/app/dict/table-view/result-table/result-table.component.ts
@@ -1,6 +1,7 @@
 // SPDX-FileCopyrightText: © 2022 Michael Köther <mkoether38@gmail.com>
 // SPDX-License-Identifier: AGPL-3.0-only
 import { Component, OnInit } from '@angular/core';
+import { DEFAULT_UI_LOCALE } from '@app/_config/config';
 import { LemmaService } from '@app/_services/lemma.service';
 import { SearchService } from '@app/_services/search.service';
 
@@ -11,10 +12,12 @@ import { SearchService } from '@app/_services/search.service';
 })
 export class ResultTableComponent implements OnInit
 {
+	DEFAULT_UI_LOCALE = DEFAULT_UI_LOCALE;
+
 	constructor(
 		public search: SearchService,
-		public lemma: LemmaService)
-	{
+		public lemma: LemmaService,
+	) {
 	}
 
 	ngOnInit(): void
