f79f6920732e33e972f3b58a5cc103f9aaeace87
mikoet <mkoether38@gmail.com>
2022-12-30 01:58:04 +0100

    Implemented functionality to set meta tags for social media dynamically from UI translations via UI scope 'metaTags'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,21 @@
 // SPDX-FileCopyrightText: © 2022 Michael Köther <mkoether38@gmail.com>
-// SPDX-License-Identifier: AGPL-3.0-only
-import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
-import { DOCUMENT } from '@angular/common';
+import { ReplaySubject } from 'rxjs';
+import { take, takeUntil } from 'rxjs/operators';
 
+import { DOCUMENT } from '@angular/common';
+// SPDX-License-Identifier: AGPL-3.0-only
+import { Component, Inject, NgZone, OnDestroy, OnInit } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
 import { NavigationEnd, Router } from '@angular/router';
 import { TranslocoService } from '@ngneat/transloco';
-import { ReplaySubject, Subscription } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
+
 import { DEFAULT_UI_LOCALE } from './_config/config';
 
 /**
  * The AppComponent handles:
- * 1. The locale part within the URL to be in sync with the set active language: /[locale]/some/component.
- * 2. So put a locale part into the URL if there is none yet.
+ * 1. The locale part within the URL to be in sync with the set active language: /[locale]/some/path.
+ * 2. So it puts a locale part into the URL if there is none yet.
+ * 3. Will add / update meta tags to the head tag of index.html. The tags will be taken from UI translation scope 'metaTags'.
  */
 @Component({
 	selector: 'app-root',
@@ -21,18 +24,19 @@ import { DEFAULT_UI_LOCALE } from './_config/config';
 })
 export class AppComponent implements OnInit, OnDestroy
 {
-	private destroy$ = new ReplaySubject();
+	private destroy$ = new ReplaySubject<void>(1);
 
 	constructor(
 		/*public configService: ConfigService,*/
 		@Inject(DOCUMENT) private document: Document,
 		private router: Router,
+		private zone: NgZone,
 		public transloco: TranslocoService,
+		private meta: Meta,
 	) {
 	}
 
-	ngOnInit(): void
-	{
+	ngOnInit(): void {
 		this.router.events.pipe(
 			takeUntil(this.destroy$),
 		).subscribe(event => {
@@ -51,12 +55,20 @@ export class AppComponent implements OnInit, OnDestroy
 						parts[1] = DEFAULT_UI_LOCALE;
 						url = parts.join('/');
 					}
-					this.transloco.setActiveLang(parts[1]);
-					// TODO this.configService.uiLanguage = this.translate.currentLang;
-					this.router.navigateByUrl(url);
+
+					this.zone.run(async () => {
+						const locale = parts[1];
+						if (locale !== this.transloco.getActiveLang()) {
+							// Don't trigger unnecessary 'second change' to same locale
+							this.transloco.setActiveLang(locale);
+						}
+						// TODO this.configService.uiLanguage = this.translate.currentLang;
+						await this.router.navigateByUrl(url);
+						this.initMeta();
+					});
 				} else {
 					let locale = this.transloco.getActiveLang();
-					this.router.navigateByUrl(`/${locale}`);
+					this.zone.run(async () => await this.router.navigateByUrl(`/${locale}`));
 				}
 			}
 		});
@@ -71,21 +83,42 @@ export class AppComponent implements OnInit, OnDestroy
 		this.setHtmlLang(this.transloco.getActiveLang());
 	}
 
-	private setHtmlLang(locale: string) : void
-	{
-		// Change the value of the <html lang="..."> attribute
-		this.document.documentElement.lang = locale;
-	}
-
-	ngOnDestroy(): void
-	{
+	ngOnDestroy(): void {
 		this.destroy$.next();
 		this.destroy$.complete();
 	}
 
-	toggleTheme()
-	{
+	toggleTheme() {
 		this.document.body.classList.toggle('dark-theme');
 		this.document.body.classList.toggle('light-theme');
+	}
+
+	private setHtmlLang(locale: string): void {
+		// Change the value of the <html lang="..."> attribute
+		this.document.documentElement.lang = locale;
+	}
+
+	private initMeta(): void {
+		this.transloco.load(`metaTags/${this.transloco.getActiveLang()}`).pipe(
+			take(1),
+			takeUntil(this.destroy$),
+		).subscribe(translation => {
+			for (const key in translation) {
+				const matches = key.match(/(itemprop|name|property)='(.*)'/);
+				if (!!matches && matches.length === 3) {
+					const keyProperty: string = matches[1];
+					const keyValue: string = matches[2];
+					const content: string = translation[key];
+
+					if (this.meta.getTag(key)) {
+						this.meta.updateTag({ [keyProperty]: keyValue, content }, key);
+					} else {
+						this.meta.addTag({ [keyProperty]: keyValue, content });
+					}
+				} else {
+					console.warn('Retreived invalid meta key', key);
+				}
+			}
+		});
 	}
 }
