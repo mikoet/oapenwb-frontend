2d1713d4310415900bb8f5e658982febbf27cb13
mikoet <mkoether38@gmail.com>
2023-02-10 05:46:41 +0100

    Small style changes, mostly to give a bit better experience on mobile devices

--- src/app/dict/header/header.component.ts
+++ src/app/dict/header/header.component.ts
@@ -1,9 +1,8 @@
 // SPDX-FileCopyrightText: © 2022 Michael Köther <mkoether38@gmail.com>
 // SPDX-License-Identifier: AGPL-3.0-only
-import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
-import { TranslocoService } from '@ngneat/transloco';
-
+import { Component, OnInit } from '@angular/core';
 import { faLanguage } from '@fortawesome/free-solid-svg-icons';
+import { TranslocoService } from '@ngneat/transloco';
 
 @Component({
 	selector: 'dict-header',
@@ -16,7 +15,6 @@ export class HeaderComponent implements OnInit {
 
 	constructor(
 		public transloco: TranslocoService,
-		private changeDetector: ChangeDetectorRef
 	) { }
 
 	ngOnInit(): void {
