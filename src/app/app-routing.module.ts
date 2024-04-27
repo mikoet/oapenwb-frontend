3ce913d4123206a65a97663942d7799b82da8a61
mikoet <mkoether38@gmail.com>
2024-04-27 18:40:23 +0200

    Value for initialNavigation changed with update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -64,7 +64,7 @@ const routes: Routes = [
 
 @NgModule({
 	imports: [RouterModule.forRoot(routes, {
-		initialNavigation: 'enabled'
+		initialNavigation: 'enabledNonBlocking',
 })],
 	exports: [RouterModule]
 })
