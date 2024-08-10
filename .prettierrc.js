ed5feb64dd536dc51804c51f137ca1e121d105fd
mikoet <mkoether38@gmail.com>
2024-08-10 21:51:22 +0200

    Installed and configured prettier

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    bracketSameLine: false,
+    bracketSpacing: true,
+    embeddedLanguageFormatting: "off",
+    printWidth: 125,
+    semi: false,
+    singleAttributePerLine: true,
+    tabWidth: 4,
+    trailingComma: "all",
+    useTabs: true,
+};
