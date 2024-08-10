ac37fd759125bec31257fb86f7b59d180c7a4c5a
mikoet <mkoether38@gmail.com>
2024-08-10 21:51:48 +0200

    Formatted AdminComponent for a test

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -2,31 +2,59 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 import { MediaMatcher } from '@angular/cdk/layout';
 import { DOCUMENT, NgFor } from '@angular/common';
-import { ChangeDetectorRef, Component, Inject, isDevMode, OnInit, OnDestroy } from '@angular/core';
+import {
+	ChangeDetectorRef,
+	Component,
+	Inject,
+	OnInit,
+	OnDestroy,
+} from '@angular/core';
 import { Router, RouterLink, RouterOutlet } from '@angular/router';
-
 import { TranslocoService, TranslocoDirective } from '@jsverse/transloco';
 
 import { AccountService } from '@app/shared/_services/account.service';
-import { getRouteStrWithoutLang, RoutingPipe } from '@app/shared/_pipes/routing.pipe';
+import {
+	getRouteStrWithoutLang,
+	RoutingPipe,
+} from '@app/shared/_pipes/routing.pipe';
 import { ROUTE_LOGIN } from '@app/routes';
 import { BlockUIModule } from 'ng-block-ui';
 import { SideMenuComponent } from './side-menu/side-menu.component';
-import { MatSidenavContainer, MatSidenav, MatSidenavContent } from '@angular/material/sidenav';
+import {
+	MatSidenavContainer,
+	MatSidenav,
+	MatSidenavContent,
+} from '@angular/material/sidenav';
 import { MatMenuTrigger, MatMenu, MatMenuItem } from '@angular/material/menu';
 import { MatIcon } from '@angular/material/icon';
 import { MatIconButton } from '@angular/material/button';
 import { MatToolbar } from '@angular/material/toolbar';
 
 @Component({
-    selector: 'app-admin',
-    templateUrl: './admin.component.html',
-    styleUrls: ['./admin.component.scss'],
-    standalone: true,
-    imports: [TranslocoDirective, MatToolbar, MatIconButton, MatIcon, RouterLink, MatMenuTrigger, MatMenu, MatMenuItem, NgFor, MatSidenavContainer, MatSidenav, SideMenuComponent, MatSidenavContent, BlockUIModule, RouterOutlet, RoutingPipe]
+	selector: 'app-admin',
+	templateUrl: './admin.component.html',
+	styleUrls: ['./admin.component.scss'],
+	standalone: true,
+	imports: [
+		TranslocoDirective,
+		MatToolbar,
+		MatIconButton,
+		MatIcon,
+		RouterLink,
+		MatMenuTrigger,
+		MatMenu,
+		MatMenuItem,
+		NgFor,
+		MatSidenavContainer,
+		MatSidenav,
+		SideMenuComponent,
+		MatSidenavContent,
+		BlockUIModule,
+		RouterOutlet,
+		RoutingPipe,
+	],
 })
-export class AdminComponent implements OnInit, OnDestroy
-{
+export class AdminComponent implements OnInit, OnDestroy {
 	mobileQuery: MediaQueryList;
 	private _mobileQueryListener: () => void;
 
@@ -39,8 +67,8 @@ export class AdminComponent implements OnInit, OnDestroy
 		@Inject(DOCUMENT) private document: Document,
 		public transloco: TranslocoService,
 		private router: Router,
-		private account: AccountService)
-	{
+		private account: AccountService,
+	) {
 		this.mobileQuery = media.matchMedia('(max-width: 1000px)');
 		this._mobileQueryListener = () => changeDetectorRef.detectChanges();
 		this.mobileQuery.addEventListener('change', this._mobileQueryListener);
@@ -48,13 +76,13 @@ export class AdminComponent implements OnInit, OnDestroy
 		this.loadStyle('styles-admin');
 	}
 
-	ngOnInit(): void
-	{
-	}
+	ngOnInit(): void {}
 
-	ngOnDestroy(): void
-	{
-		this.mobileQuery.removeEventListener('change', this._mobileQueryListener);
+	ngOnDestroy(): void {
+		this.mobileQuery.removeEventListener(
+			'change',
+			this._mobileQueryListener,
+		);
 
 		// Remove the theme link element from Head
 		let themeLink = this.document.getElementById('client-theme');
@@ -68,12 +96,11 @@ export class AdminComponent implements OnInit, OnDestroy
 	//      does not correctly reach the browser
 	// from: https://juristr.com/blog/2019/08/dynamically-load-css-angular-cli/
 	// TODO !!! Das Elemente bei ngOnDestroy() wieder entfernen.
-	private loadStyle(styleName: string)
-	{
+	private loadStyle(styleName: string) {
 		const head = this.document.getElementsByTagName('head')[0];
 
 		let themeLink = this.document.getElementById(
-			'client-theme'
+			'client-theme',
 		) as HTMLLinkElement;
 		if (themeLink) {
 			// TODO Unterscheidung nach script/style, und auch hier .js?
@@ -91,27 +118,27 @@ export class AdminComponent implements OnInit, OnDestroy
 
 				head.appendChild(script);
 			} else {*/
-				const style = this.document.createElement('link');
-				style.id = 'client-theme';
-				style.rel = 'stylesheet';
-				style.href = `${styleName}.css`;
-	
-				head.appendChild(style);
+			const style = this.document.createElement('link');
+			style.id = 'client-theme';
+			style.rel = 'stylesheet';
+			style.href = `${styleName}.css`;
+
+			head.appendChild(style);
 			//}
 		}
 	}
 
-	onUiLanguageClicked(lang: string): void
-	{
+	onUiLanguageClicked(lang: string): void {
 		this.transloco.setActiveLang(lang);
 	}
 
-	onLogoutClicked(): void
-	{
+	onLogoutClicked(): void {
 		this.account.logout();
 		// Redirect to login page
 		let lang = this.transloco.getActiveLang();
 		let url = `/${lang}/` + getRouteStrWithoutLang(ROUTE_LOGIN);
-		this.router.navigate([url], { queryParams: { returnURL: this.router.routerState.snapshot.url } });
+		this.router.navigate([url], {
+			queryParams: { returnURL: this.router.routerState.snapshot.url },
+		});
 	}
 }
